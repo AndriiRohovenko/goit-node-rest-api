bbb76f6548cce3f1db2bd3dc21daa9738301439a
Andrii Rohovenko <andrii.rohovenko@gmail.com>
2025-11-18 23:27:43 +0200

    add Contact model, updated Contacts service

--- db/connection.js
+++ db/connection.js
@@ -4,9 +4,10 @@ const connectDB = async () => {
   try {
     console.log();
     await sequelize.authenticate();
-    console.log("Database connection has been established successfully.");
+    console.log("Database connection successful");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
+    process.exit(1);
   }
 };
 
